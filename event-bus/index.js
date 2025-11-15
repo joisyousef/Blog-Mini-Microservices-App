664714783819d4f3b7f86b920178284c37acd1c7
Yousef Elsrogy <yousefelsrogy500@gmail.com>
2025-11-16 00:23:44 +0200

    Fixing errors and Receiving events on posts and comments created

--- event-bus/index.js
+++ event-bus/index.js
@@ -3,18 +3,29 @@ const bodyParser = require("body-parser");
 const axios = require("axios");
 
 const app = express();
-
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   const event = req.body;
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
+
+  const services = [
+    "http://localhost:4000/events", // posts service
+    "http://localhost:4001/events", // comments service
+    "http://localhost:4002/events", // query service
+  ];
+
+  for (let url of services) {
+    try {
+      await axios.post(url, event);
+    } catch (err) {
+      console.log(`❌ Error sending event to ${url}`);
+      console.log(err.message);
+    }
+  }
 
   res.send({ status: "OK" });
 });
 
 app.listen(4005, () => {
-  console.log("Listening on port 4005");
+  console.log("Event Bus listening on 4005");
 });
