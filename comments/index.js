72081ec5d0038b4d76e4fc36604de222b21be259
Yousef Elsrogy <yousefelsrogy500@gmail.com>
2025-11-17 23:20:31 +0200

    Adding Comment Moderation

--- comments/index.js
+++ comments/index.js
@@ -20,7 +20,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = comentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "Pending" });
 
   comentsByPostId[req.params.id] = comments;
 
@@ -30,6 +30,7 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "Pending",
     },
   });
 
